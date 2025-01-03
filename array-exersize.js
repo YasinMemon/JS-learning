8f3bbb95029601fd445ce40f15d3cf05539cc15c
YasinMemon <memonyasin2007@gmail.com>
2025-01-03 17:47:09 +0530

    array exersize done

---
+++ array-exersize.js
@@ -0,0 +1,11 @@
+// Exercise: Arrays
+// For this exercise, your task is to push two new numbers onto the existing numbers array: 8 and 2, in that order.
+
+// Don't create a new array, don't overwrite the existing array - instead manipulate the existing array as taught earlier in the course.
+
+const existArr = [8, 2];
+
+existArr.push(10);
+existArr.push(24);
+
+console.log(existArr);
